b60fd9c2ba6b9b80ebce8034d9a89805bcac7241
denismironchuk <SecretSecret1!>
2020-03-30 01:34:47 +0300

    beautiful permutations. reduced mod count

--- taraBeautifulPermutatios.py
+++ taraBeautifulPermutatios.py
@@ -1,3 +1,6 @@
+import sys
+from datetime import datetime
+
 MOD = 10 ** 9 + 7
 TWO = (MOD + 1) // 2
 
@@ -9,22 +12,28 @@ def fact(n):
     return res
 
 def calc(prev, cur, next, i, len):
-    return (((((((next * (i + 1)) % MOD) + ((cur * (len - i - 1))) % MOD) % MOD) * TWO) % MOD) + prev) % MOD
+    return (((((((next * (i + 1))) + ((cur * (len - i - 1))))) * TWO)) + prev) % MOD
 
 if __name__ == '__main__':
-    q = int(input())
+    with open('C:\\Users\\dmiro\\test.txt', 'r') as sys.stdin:
+        q = int(input())
 
-    for _ in range(q):
-        n = int(input())
-        a = list(map(int, input().split()))
-        a.sort()
-        doubLen = len([a[i] for i in range(n - 1) if a[i] == a[i + 1]])
-        elmntLen = n - doubLen
+        d1 = datetime.now()
+        for _ in range(q):
+            n = int(input())
+            a = list(map(int, input().split()))
+            a.sort()
+            #doubLen = len([_ for cur, next in zip(a, a[1:]) if cur == next])
+            doubLen = len([a[i] for i in range(n - 1) if a[i] == a[i + 1]])
+            elmntLen = n - doubLen
+            res = [0, 0, fact(elmntLen), 0, 0]
 
-        res = [0, 0, fact(elmntLen), 0, 0]
+            for i in range(doubLen):
+                res = [0,0] + [calc(prev, cur, next, index, elmntLen) for prev, cur, next, index in zip(res, res[1:], res[2:], range(-1, i + 3))][1:] + [0,0]
+                elmntLen += 1
 
-        for i in range(doubLen):
-            res = [0,0] + [calc(prev, cur, next, index, elmntLen) for prev, cur, next, index in zip(res, res[1:], res[2:], range(-1, i + 3))][1:] + [0,0]
-            elmntLen += 1
+            print(res[2])
 
-        print(res[2])
+        d2 = datetime.now()
+
+        print((d2 - d1).total_seconds())
