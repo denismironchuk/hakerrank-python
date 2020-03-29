1ddbddedaa501d89a67860955a0592a1d6f08ff5
denismironchuk <SecretSecret1!>
2020-03-30 00:55:46 +0300

    beautiful permutations. use zip()

--- taraBeautifulPermutatios.py
+++ taraBeautifulPermutatios.py
@@ -8,6 +8,9 @@ def fact(n):
 
     return res
 
+def calc(prev, cur, next, i, len):
+    return (((((((next * (i + 1)) % MOD) + ((cur * (len - i - 1))) % MOD) % MOD) * TWO) % MOD) + prev) % MOD
+
 if __name__ == '__main__':
     q = int(input())
 
@@ -18,22 +21,10 @@ if __name__ == '__main__':
         doubLen = len([a[i] for i in range(n - 1) if a[i] == a[i + 1]])
         elmntLen = n - doubLen
 
-        res = [fact(elmntLen)] + [0] * (doubLen + 1)
+        res = [0, 0, fact(elmntLen), 0, 0]
 
         for i in range(doubLen):
-            prev = 0
-            for i in range(0, i + 2):
-                newVal = (((res[i + 1] * (i + 1)) % MOD) + ((res[i] * (elmntLen - i - 1))) % MOD) % MOD
-
-                newVal *= TWO
-                newVal %= MOD
-
-                newVal += prev
-                newVal %= MOD
-
-                prev = res[i]
-                res[i] = newVal
-
+            res = [0,0] + [calc(prev, cur, next, index, elmntLen) for prev, cur, next, index in zip(res, res[1:], res[2:], range(-1, i + 3))][1:] + [0,0]
             elmntLen += 1
 
-        print(res[0])
+        print(res[2])
